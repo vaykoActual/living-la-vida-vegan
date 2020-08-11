010e29c3fdadf3e00734d18f36cd7306b174c7fe
Zulay Scottborgh <zscottborgh@Zulays-MacBook-Pro.local>
2020-08-11 11:55:17 -0400

    Initiating authentication for user. updated gems to include bcrypt and jwt. added migration to update user table with password_digest column

--- living-la-vida-vegan/app/controllers/users_controller.rb
+++ living-la-vida-vegan/app/controllers/users_controller.rb
@@ -16,9 +16,9 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
     if @user.save
-      render json: @user, status: :created
+      @token = encode({ id: @user.id })
+      render json: { user: @user.attributes.except("password_digest"), token: @token }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :email, :password, :img_url)
+      params.require(:user).permit(:username, :email, :password)
     end
 end
--- living-la-vida-vegan/app/models/user.rb
+++ living-la-vida-vegan/app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_secure_password
+  
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
