6926f0b43a40a2c594f6509072045dad8823e7dd
Zulay Scottborgh <zscottborgh@Zulays-MacBook-Pro.local>
2020-08-12 23:01:47 -0400

    updated UpdateRecipe components

--- living-la-vida-vegan/client/src/components/Recipes/UpdateRecipe/UpdateRecipe.jsx
+++ living-la-vida-vegan/client/src/components/Recipes/UpdateRecipe/UpdateRecipe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { putRecipe } from '../../../services/recipes';
 
@@ -14,22 +14,45 @@ export default function UpdateRecipe(props) {
     source: '',
   });
 
+  useEffect(() => {
+    defaultRecipeData();
+  }, [props.recipes]);
+
   // const [clicked, setClicked] = useState({
   //   clicked: false,
   // });
 
+  const defaultRecipeData = () => {
+    const recipe = props.recipes.find((recipe) => {
+      return recipe.id === props.match.params.id;
+    });
+    if (recipe) {
+      const { name, value } = e.target;
+      setRecipeUpdate({
+        ...recipeUpdate,
+        [name]: value,
+      });
+    }
+  };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setRecipeUpdate({
+      ...recipeUpdate,
       [name]: value,
     });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const recipeData = await putRecipe(recipeUpdate);
-    // props.setCurrentUser(userData);
-    props.history.push('/');
+    const { id } = props.match.params;
+    const newRecipe = await putRecipe(id, recipeUpdate);
+    props.setRecipes(
+      props.recipes.map((recipe) => {
+        return recipe.id === parseInt(id) ? newRecipe : recipe;
+      })
+    );
+    props.history.push('/recipes');
   };
 
   // const handleClick = () => {
@@ -39,7 +62,7 @@ export default function UpdateRecipe(props) {
   return (
     <div>
       <form className='add-recipe' onSubmit={handleSubmit}>
-        <h3>Add a Recipe</h3>
+        <h3>Update Recipe</h3>
         <input
           type='text'
           name='upload-photo'
