107ee7d35e6ff10a5ddb04671d709671ce89c3b2
Zulay Scottborgh <zscottborgh@Zulays-MacBook-Pro.local>
2020-08-12 22:38:15 -0400

    create recipe form in progress

--- living-la-vida-vegan/client/src/components/Recipes/CreateRecipe/CreateRecipe.jsx
+++ living-la-vida-vegan/client/src/components/Recipes/CreateRecipe/CreateRecipe.jsx
@@ -1,5 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { postRecipe } from '../../../services/recipes';
 
-export default function DeleteRecipes() {
-  return <div></div>;
+export default function CreateRecipe(props) {
+  const [recipeInput, setRecipeInput] = useState({
+    uploadPhoto: '',
+    recipeName: '',
+    description: '',
+    prepTime: '',
+    cookTime: '',
+    ingredients: '',
+    stepsInstructions: '',
+    source: '',
+  });
+
+  // const [clicked, setClicked] = useState({
+  //   clicked: false,
+  // });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRecipeInput({
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const recipeData = await postRecipe(recipeInput);
+    // props.setCurrentUser(userData);
+    props.history.push('/');
+  };
+
+  // const handleClick = () => {
+  //   setClicked(!clicked);
+  // };
+
+  return (
+    <div>
+      <form className='add-recipe' onSubmit={handleSubmit}>
+        <h3>Add a Recipe</h3>
+        <input
+          type='text'
+          name='upload-photo'
+          value={recipeInput.uploadPhoto}
+          onChange={handleChange}
+          placeholder='Upload a Photo'
+        />
+        <input
+          type='text'
+          name='recipe-name'
+          value={recipeInput.recipeName}
+          onChange={handleChange}
+          placeholder='Recipe Name'
+        />
+        <input
+          type='text'
+          name='description'
+          value={recipeInput.description}
+          onChange={handleChange}
+          placeholder='Description'
+        />
+        <input
+          type='text'
+          name='prep-time'
+          value={recipeInput.prepTime}
+          onChange={handleChange}
+          placeholder='Prep Time'
+        />
+        <input
+          type='text'
+          name='cook-time'
+          value={recipeInput.cookTime}
+          onChange={handleChange}
+          placeholder='Cook Time'
+        />
+        <input
+          type='text'
+          name='ingredients'
+          value={recipeInput.ingredients}
+          onChange={handleChange}
+          placeholder='Ingredients'
+        />
+        <input
+          type='text'
+          name='steps-instructions'
+          value={recipeInput.stepsInstructions}
+          onChange={handleChange}
+          placeholder='Steps/Instructions'
+        />
+        <input
+          type='text'
+          name='source'
+          value={recipeInput.source}
+          onChange={handleChange}
+          placeholder='Source'
+        />
+        <Link to='/recipe'>
+          <button>Save</button>
+        </Link>
+      </form>
+    </div>
+  );
 }
